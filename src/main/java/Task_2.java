bb6aac6eb18826f3eceddd9bc06df842ca007a3c
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-04 17:07:59 +0200

    Task_2.java

--- src/main/java/Task_2.java
+++ src/main/java/Task_2.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Task_2 {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+public class Task_2 {
+    public static void main(String[] args) {
+        List<String> input = Arrays.asList("Vika", "Vitalik", "Mikita", "Weronika", "Carlos", "Max", "Olha", "Susi", "Bars");
+
+
+        List<String> output = input.stream()
+                .map(String::toUpperCase)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+        System.out.println(output);
+
+    }
+
 }
