097c3b91cc5ef15d322d2dc710582eeed399f90f
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-04 17:08:56 +0200

    Update Task_4.java

--- src/main/java/Task_4.java
+++ src/main/java/Task_4.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Task_4 {
+import java.util.stream.Stream;
+public class Task_4 {
+    public static void main(String[] args) {
+        long a = 25214903917L;
+        long c = 11L;
+        long m = (long) Math.pow(2, 48);
+        long seed = 0;
+
+
+        Stream<Long> randomLongNumbers = Stream.iterate(seed, (n) -> linearCongruentGenerator(a, c, m, n));
+        randomLongNumbers
+                .limit(10)
+                .peek(System.out::println)
+                .count();
+
+    }
+
+    static long linearCongruentGenerator (long a, long c, long m, long randomNumber) {
+        return (a * randomNumber + c) % m;
+    }
+
 }
