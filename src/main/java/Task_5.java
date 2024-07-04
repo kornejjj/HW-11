6414c89d13418be87fd6d11eaa96ab6c993fe0e3
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-04 22:26:17 +0200

    Task_5.java

--- src/main/java/Task_5.java
+++ src/main/java/Task_5.java
@@ -1,25 +1,29 @@
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Task_5 {
     public static void main(String[] args) {
-        Stream<Integer> first = Stream.of(12, 16, 19, -9);
-        Stream<Integer> second = Stream.of(12);
+        
 
-        zip(first, second).forEach(System.out::println);
+
+        Supplier<Stream<String>> streamSupplier1 = () -> Stream.of("A", "B", "C");
+        Supplier<Stream<String>> streamSupplier2 = () -> Stream.of("1", "2", "3", "4");
+        
+        
+        Stream<String> result2 = zip(streamSupplier1.get(), streamSupplier2.get());
+        System.out.println(result2.collect(Collectors.toList()));
     }
 
-    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
-        List<T> resultList = new ArrayList<>();
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second){
         Iterator<T> firstIterator = first.iterator();
         Iterator<T> secondIterator = second.iterator();
-        Stream.iterate(0, i -> firstIterator.hasNext() && secondIterator.hasNext(), i -> i + 1)
-                .forEach(i -> {
-                    resultList.add(firstIterator.next());
-                    resultList.add(secondIterator.next());
-                });
-        return resultList.stream();
+        Stream.Builder<T> builder = Stream.builder();
+        while (firstIterator.hasNext() && secondIterator.hasNext()) {
+            builder.add(firstIterator.next());
+            builder.add(secondIterator.next());
+        }
+        return builder.build();
     }
 }
