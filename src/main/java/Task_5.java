f01c756b8a8aa99cba800516cf7b8273d854f015
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-04 17:09:21 +0200

    Update Task_5.java

--- src/main/java/Task_5.java
+++ src/main/java/Task_5.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Task_5 {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        Stream<Integer> first = Stream.of(12, 16, 19, -9);
+        Stream<Integer> second = Stream.of(12);
+
+        zip(first, second).forEach(System.out::println);
+    }
+
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        List<T> resultList = new ArrayList<>();
+        Iterator<T> firstIterator = first.iterator();
+        Iterator<T> secondIterator = second.iterator();
+        Stream.iterate(0, i -> firstIterator.hasNext() && secondIterator.hasNext(), i -> i + 1)
+                .forEach(i -> {
+                    resultList.add(firstIterator.next());
+                    resultList.add(secondIterator.next());
+                });
+        return resultList.stream();
+    }
 }
