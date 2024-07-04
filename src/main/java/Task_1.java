8dc3930b84a26cea770fcf44373b63cb59f5c2a0
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-04 22:16:51 +0200

    Task_1.java

--- src/main/java/Task_1.java
+++ src/main/java/Task_1.java
@@ -1,25 +1,17 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Task_1 {
     public static void main(String[] args) {
         List<String> input = Arrays.asList("Vika", "Vitalik", "Mikita", "Weronika", "Carlos", "Max", "Olha", "Susi", "Bars");
 
-        AtomicInteger number = new AtomicInteger(0);
-        String output = input.stream()
-                .map(name -> {
-                    if ((number.get() % 2) != 0) {
-                        return number.getAndIncrement() + ". " + name;
-                    } else {
-                        number.getAndIncrement();
-                        return "";
-                    }
-                })
-                .filter(name -> !name.isEmpty())
-                .collect(Collectors.joining(", "));
-        System.out.println(output);
+        System.out.println(filterNames(input));
+    }
+
+    static String filterNames(List<String> names) {
+        return names.stream().map((i) -> names.indexOf(i) % 2 != 0 ? names.indexOf(i) + ". " + i + ", " : "")
+                .collect(Collectors.joining());
 
     }
 
