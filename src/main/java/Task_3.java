085c3dc8ce72f3a5d5b7470c2406ede981e8f0e7
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-04 17:08:28 +0200

    Update Task_3.java

--- src/main/java/Task_3.java
+++ src/main/java/Task_3.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Task_3 {
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        String[] array = {"1, 2, 0", "4, 5"};
+
+
+        String output = Stream.of(array)
+                .flatMap(s -> Stream.of(s.split(", ")))
+                .map(String::trim)
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .mapToObj(Integer::toString)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(output);
+
+
+    }
+
 }
