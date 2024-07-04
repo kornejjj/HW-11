18d01656c37292ba103a7b6e3c9d42a94dd0eb47
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-04 17:07:09 +0200

    Task_1.java

--- src/main/java/Task_1.java
+++ src/main/java/Task_1.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Task_1 {
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        List<String> input = Arrays.asList("Vika", "Vitalik", "Mikita", "Weronika", "Carlos", "Max", "Olha", "Susi", "Bars");
+
+        AtomicInteger number = new AtomicInteger(0);
+        String output = input.stream()
+                .map(name -> {
+                    if ((number.get() % 2) != 0) {
+                        return number.getAndIncrement() + ". " + name;
+                    } else {
+                        number.getAndIncrement();
+                        return "";
+                    }
+                })
+                .filter(name -> !name.isEmpty())
+                .collect(Collectors.joining(", "));
+        System.out.println(output);
+
+    }
+
+
 }
