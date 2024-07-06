adc87a283221af5dee279dec23b6fe2b3989a66e
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-06 11:30:36 +0200

    Task_1.java

--- src/main/java/Task_1.java
+++ src/main/java/Task_1.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Task_1 {
     public static void main(String[] args) {
@@ -10,10 +11,9 @@ public class Task_1 {
     }
 
     static String filterNames(List<String> names) {
-        return names.stream().map((i) -> names.indexOf(i) % 2 != 0 ? names.indexOf(i) + ". " + i + ", " : "")
+        return IntStream.range(0, names.size())
+                .filter(i -> i % 2 != 0) 
+                .mapToObj(i -> i + ". " + names.get(i) + ", ")
                 .collect(Collectors.joining());
-
     }
-
-
 }
