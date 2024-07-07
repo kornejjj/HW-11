73cf9c05a2194d2bc6b1a0e1f44b2884983eec45
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-07-07 22:19:39 +0200

    Update Task_5.java

--- src/main/java/Task_5.java
+++ src/main/java/Task_5.java
@@ -5,25 +5,22 @@ import java.util.stream.Stream;
 
 public class Task_5 {
     public static void main(String[] args) {
-        
+
 
 
         Supplier<Stream<String>> streamSupplier1 = () -> Stream.of("A", "B", "C");
         Supplier<Stream<String>> streamSupplier2 = () -> Stream.of("1", "2", "3", "4");
-        
-        
+
+
         Stream<String> result2 = zip(streamSupplier1.get(), streamSupplier2.get());
         System.out.println(result2.collect(Collectors.toList()));
     }
 
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second){
-        Iterator<T> firstIterator = first.iterator();
+
         Iterator<T> secondIterator = second.iterator();
-        Stream.Builder<T> builder = Stream.builder();
-        while (firstIterator.hasNext() && secondIterator.hasNext()) {
-            builder.add(firstIterator.next());
-            builder.add(secondIterator.next());
-        }
-        return builder.build();
+
+
+        return first.filter(ignore -> secondIterator.hasNext()).flatMap(f -> Stream.of(f, secondIterator.next()));
     }
 }
